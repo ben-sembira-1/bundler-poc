f26729d7a4f75cb45c6c51f5b0b51290d04aed9a
Ben <ben.sembira.1@gmail.com>
2024-06-29 10:53:00 +0300

    fix: more verbose way to require configuration_files field

--- modules/models.py
+++ modules/models.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from typing import List, Union
-from pydantic import BaseModel, field_validator
+from typing import Any, List, Union
+from pydantic import BaseModel, field_validator, model_validator
 
 
 class Dependency(BaseModel):
@@ -12,16 +12,27 @@ class Dependency(BaseModel):
 class Module(BaseModel):
     name: str
     dependencies: List[Dependency]
-    configuration_files: None
 
     @field_validator('name')
     @classmethod
-    def name_matches_classname(cls, name: str) -> str:
+    def match_name_to_classname(cls, name: str) -> str:
         currently_validated_module_name = cls.__name__
         assert (
             name == currently_validated_module_name
         ), f"Could not find module named '{name}'. If you recieved this error you can ignore other pydantic errors."
         return name
+
+    @model_validator(mode='before')
+    @classmethod
+    def subclass_defined_configuration_files(cls, data: Any) -> Any:
+        REQUIRED_FIELD_NAME = 'configuration_files'
+        assert issubclass(
+            cls, Module
+        ), f"Shoud never get to here, internal pydantic error or missunderstanding the way pydantic works"
+        assert (
+            REQUIRED_FIELD_NAME in cls.model_fields
+        ), f"Model {cls} should define the '{REQUIRED_FIELD_NAME}' field"
+        return data
 
 
 class Chrome(Module):
@@ -49,7 +60,7 @@ class MissionPlanner(Module):
 
 
 class Neptune(Module):
-    pass
+    configuration_files: None
 
 
 class LogsShortcuts(Module):
@@ -57,6 +68,7 @@ class LogsShortcuts(Module):
         log_paths: List[Path]
 
     parameters: Parameters
+    configuration_files: None
 
 
 ModulesAvailable = Union[Chrome, MissionPlanner, Neptune, LogsShortcuts]
